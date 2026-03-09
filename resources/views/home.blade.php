5e3f209c3de77623027d57b7b10a3677f3a2aa9e
Reyvandito Bassam Camilo <ditorevan55@gmail.com>
2026-03-09 18:04:05 +0700

    P2

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<div> 
+    <!-- Simplicity is an acquired taste. - Katharine Gerould --> 
+Selamat Datang di Rental kendaraan Terbaik {{$nama}}<br> 
+Anda adalah seorang {{$pekerjaan}} 
+</div>  
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
+//Route::get('/', function () {
+// return view('welcome');
+//});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/contact', [HomeController::class, 'contact']);
